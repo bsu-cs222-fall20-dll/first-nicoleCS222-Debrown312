9cf8d5d56f1d088c2fe5c6df49994de5055a9cb5
David Brown <debrown312@gmail.com>
2020-09-21 15:10:12 -0400

    Attempt using Json

--- src/test/java/JsonLearningTest.java
+++ src/test/java/JsonLearningTest.java
@@ -1,15 +1,30 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Map;
 
 public class JsonLearningTest {
+    @SuppressWarnings("deprecation")
     @Test
     public void testCountRevisions(){
         JsonParser parser = new JsonParser();
-        ImputStream inputStream = getClass().getClassLoader().getResourceAsStream("sample.json");
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("sample.json");
         Reader reader = new InputStreamReader(inputStream);
+        JsonElement rootElement = parser.parse(reader);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
+        JsonArray array = null;
+        for(Map.Entry<String,JsonElement> entry : pages.entrySet()){
+            JsonObject entryObject = entry.getValue().getAsJsonObject();
+            array = entryObject.getAsJsonArray("revisions");
+        }
+        Assert.assertEquals(4, array.size());
     }
 }
