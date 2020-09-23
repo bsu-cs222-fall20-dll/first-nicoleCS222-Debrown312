3237dcdd6694d217618538b570604e4ce5b70702
Nicole Gerber <nicolecs222@gmail.com>
2020-09-23 13:37:46 -0400

    started RevisionParser class

--- src/main/java/RevisionParser.java
+++ src/main/java/RevisionParser.java
@@ -1,2 +1,14 @@
+
 public class RevisionParser {
+    public static void main(String[] args) {
+        throws InterruptedException {
+            RevisionParser Parser
+                    =new RevisionParser("Author");
+
+            try {
+
+            }
+        }
+
+    }
 }
