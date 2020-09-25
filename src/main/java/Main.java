f5087799407c03bb6d4b6e5de83be3919f5edfb4
Nicole Gerber <nicolecs222@gmail.com>
2020-09-24 20:39:48 -0400

    test revision method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,17 @@ import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) {
+        //FirstAuthor();
+        RevisionList();
 
     }
-    public void FirstAuthor() {
+    public static void FirstAuthor() {
         RevisionParser revisionParser = new RevisionParser();
         InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("sample.json");
         JsonElement firstAuthor = revisionParser.firstAuthorFinder(inputStream);
         System.out.println(firstAuthor);
     }
-    public void RevisionList() {
+    public static void RevisionList() {
         RevisionParser revisionParser = new RevisionParser();
         InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("sample.json");
         for(JsonObject entry : revisionParser.ListOfAllRevisions(inputStream)) {
