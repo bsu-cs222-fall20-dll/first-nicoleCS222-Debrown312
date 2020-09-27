2c6e026c28765f9f85eef915f1917d059b44c392
Debrown312 <56003571+Debrown312@users.noreply.github.com>
2020-09-27 16:44:47 -0400

    Attempt at fixing URL Connection Test

--- src/test/java/URLConnectionTest.java
+++ src/test/java/URLConnectionTest.java
@@ -8,16 +8,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Map;
+import java.util.Set;
 
 public class URLConnectionTest {
     @SuppressWarnings("deprecation")
     @Test
     public void URLConnectionTest() throws IOException {
         URL wiki = new URL("http://en.wikipedia.org/wiki/Zappa");
-        URLConnection connection = wiki.openConnection();
+        //URLConnection connection = wiki.openConnection();
+        HttpURLConnection connection = (HttpURLConnection) wiki.openConnection();
+        JsonElement jsonElement = new JsonParser().parse(new InputStreamReader((InputStream) connection.getContent()));
+        JsonElement pages = jsonElement.getAsJsonObject().get("query").getAsJsonObject().get("pages");
+        Set<Map.Entry<String, JsonElement>> entrySet = pages.getAsJsonObject().entrySet();
+        JsonElement yourDesiredElement = null;
+        for(Map.Entry<String,JsonElement> entry : entrySet){
+            yourDesiredElement = entry.getValue();
+        }
+        System.out.println(yourDesiredElement);
+        /*
         connection.setRequestProperty("User-Agent", "Revision Tracker/0.1 (debrown4@bsu.edu)");
         InputStream in = connection.getInputStream();
         JsonParser parser = new JsonParser();
@@ -32,5 +44,6 @@ public class URLConnectionTest {
         }
         JsonElement holder = array.get(array.size()-1);
         System.out.println(holder);
+        */
     }
 }
